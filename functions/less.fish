5511689fa0c438280c763b0676fcfa90b9ce6a93
Yozone Wang <wangyaozh@gmail.com>
2019-10-10 11:29:00 +0800

    使用空格代替 tab

--- functions/less.fish
+++ functions/less.fish
@@ -1,4 +1,4 @@
 function less --wraps less --description="Using vim's less instead of system less"
     set VIMRUNTIME (vim -e -T dumb --cmd 'exe "set t_cm=\<C-M>"|echo $VIMRUNTIME|quit' | tr -d '\015')
-	"$VIMRUNTIME/macros/less.sh" $argv
+    "$VIMRUNTIME/macros/less.sh" $argv
 end
