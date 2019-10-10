a884161b83b88f6fd82a16f03bd1523257822f39
Yozone Wang <wangyaozh@gmail.com>
2019-10-10 11:28:12 +0800

    通过命令获取 less.sh 的路径
    解决 vim 或者系统升级后 less.sh 找不到的问题

--- functions/less.fish
+++ functions/less.fish
@@ -1,4 +1,4 @@
 function less --wraps less --description="Using vim's less instead of system less"
-    /usr/share/vim/vim80/macros/less.sh $argv
+    set VIMRUNTIME (vim -e -T dumb --cmd 'exe "set t_cm=\<C-M>"|echo $VIMRUNTIME|quit' | tr -d '\015')
+	"$VIMRUNTIME/macros/less.sh" $argv
 end
-
