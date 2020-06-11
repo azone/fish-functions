129b39fc87799d7b4c51b53ab1c1b9361d8a462d
Logan Wang <logan.wang@glowing.com>
2020-06-11 17:01:58 +0800

    add fastlane alias

---
+++ functions/fastlane.fish
@@ -0,0 +1,14 @@
+function fastlane --description="execute fastlane dynamically"
+    if test -f Gemfile && grep --silent 'fastlane' Gemfile
+        bundle info fastlane 2>&1 >/dev/null
+        if test $status -ne 0
+            set_color red; echo "fastlane not installed, please execute \"bundle install\" to install." >&2
+            return 1
+        end
+        bundle exec fastlane $argv
+    else
+        set -l FASTLANE_SHELL (which fastlane)
+        $FASTLANE_SHELL $argv
+    end
+end
+
