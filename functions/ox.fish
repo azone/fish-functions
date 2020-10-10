045960361d02acb68f6e283003c8125f94afc274
Logan Wang <logan.wang@glowing.com>
2020-10-10 17:44:57 +0800

    retab

--- functions/ox.fish
+++ functions/ox.fish
@@ -1,25 +1,25 @@
 function ox --description="open Xcode project or workspace automatically"
-	if test (count $argv) -eq 0
-		set -l PROJ *.xcworkspace
-		if test -z $PROJ
-			set PROJ *.xcodeproj
-		end
+    if test (count $argv) -eq 0
+        set -l PROJ *.xcworkspace
+        if test -z $PROJ
+            set PROJ *.xcodeproj
+        end
         if test -z $PROJ
             set PROJ Package.swift
         end
-		if test -z $PROJ
-			read -n 1 -p 'set_color yellow; echo  "Xcode workspace, project file or Swift Package not found, do you still want to open Xcode?[y/n]:"' result
-			if begin test $result = "y"; or test $result = "Y"; end
-				open -a Xcode
-			end
-		else
-			open -a Xcode $PROJ >/dev/null 2>&1
-			if test $status -ne 0
-				set_color red; echo "xcworkspace, xcodeproj or Package.swift not found."
-			end
-		end
-	else
-		open -a Xcode $argv
-	end
+        if test -z $PROJ
+            read -n 1 -p 'set_color yellow; echo  "Xcode workspace, project file or Swift Package not found, do you still want to open Xcode?[y/n]:"' result
+            if begin test $result = "y"; or test $result = "Y"; end
+                open -a Xcode
+            end
+        else
+            open -a Xcode $PROJ >/dev/null 2>&1
+            if test $status -ne 0
+                set_color red; echo "xcworkspace, xcodeproj or Package.swift not found."
+            end
+        end
+    else
+        open -a Xcode $argv
+    end
 end
 
