a92655724e140e7e9f31d37020e2dd606f8a09bc
Logan Wang <wangyaozh@gmail.com>
2023-01-06 18:38:57 +0800

    fix $VIMRUNTIME for vim90

--- functions/less.fish
+++ functions/less.fish
@@ -1,5 +1,5 @@
 function less --wraps less --description="Using vim's less instead of system less"
-    set VIMRUNTIME (vim -e -T dumb --cmd 'exe "set t_cm=\<C-M>"|echo $VIMRUNTIME|quit' | tr -d '\015')
+    set VIMRUNTIME (string trim (vim -e -T dumb --cmd 'exe "set t_cm=\<C-M>"|echo $VIMRUNTIME|quit' | tr -d '\015'))
     "$VIMRUNTIME/macros/less.sh" $argv
 end
 
